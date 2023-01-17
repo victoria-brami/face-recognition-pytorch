15dcd3058a809f1b95cab91d7a3899b511fd147f
victoria-brami <victoria.brami88@gmail.com>
2023-01-17 18:07:22 +0100

    finish pipeline  version beta

--- facenet/model/trainer.py
+++ facenet/model/trainer.py
@@ -13,6 +13,10 @@ class FaceNetLitModule(pl.LightningModule):
     
     def __init__(self, 
                 net: FaceNet,
+                train_margin: float,
+                val_margin: float,
+                test_margin: float,
+                threshold: float,
                 optimizer: torch.optim.Optimizer,
                 scheduler: torch.optim.lr_scheduler,
                  ) -> None:
@@ -21,7 +25,11 @@ class FaceNetLitModule(pl.LightningModule):
 
         self.save_hyperparameters(logger=False)
         self.net = net
-        self.criterion = TripletLoss(margin=self.hparams.metrics.train_margin)
+        self.train_margin = train_margin
+        self.val_margin = val_margin
+        self.test_margin = test_margin
+        self.threshold = threshold,
+        self.criterion = TripletLoss(margin=self.train_margin)
         
         self.train_loss = MeanMetric()
         self.val_loss = MeanMetric()
@@ -32,8 +40,8 @@ class FaceNetLitModule(pl.LightningModule):
         self.train_acc = AccuracyMetric(margin=0)
         self.val_acc = AccuracyMetric(margin=0)
         self.val_acc_best = MaxMetric()
-        self.val_low_acc = AccuracyMetric(margin=self.hparams.metrics.val_margin)
-        self.test_acc = EvaluationMetric(threshold=self.hparams.metrics.threshold)
+        self.val_low_acc = AccuracyMetric(margin=self.val_margin)
+        self.test_acc = EvaluationMetric(threshold=self.threshold)
 
     def on_train_start(self):
         # by default lightning executes validation step sanity checks before training starts,
@@ -51,9 +59,9 @@ class FaceNetLitModule(pl.LightningModule):
         Examples:
             https://pytorch-lightning.readthedocs.io/en/latest/common/lightning_module.html#configure-optimizers
         """
-        optimizer = self.hparams.optimizer(params=self.parameters())
-        if self.hparams.scheduler is not None:
-            scheduler = self.hparams.scheduler(optimizer=optimizer)
+        optimizer = self.optimizer(params=self.parameters())
+        if self.scheduler is not None:
+            scheduler = self.scheduler(optimizer=optimizer)
             return {
                 "optimizer": optimizer,
                 "lr_scheduler": {
