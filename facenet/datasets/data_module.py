f3bd31c9f314cad1cc4c6648611e5f65e02cb44c
victoria-brami <victoria.brami88@gmail.com>
2023-01-17 17:42:39 +0100

    finish pipeline  version beta

--- facenet/datasets/data_module.py
+++ facenet/datasets/data_module.py
@@ -3,15 +3,26 @@ from .lfw import LFW
 from typing import Optional
 from torch.utils.data import DataLoader, random_split
 from torch import Generator
+from pathlib import Path
 
 
 
 
 class FaceTripletsDataModule(pl.LightningDataModule):
     
-    def __init__(self, hparams) -> None:
+    def __init__(self, 
+    data_dir: Path,
+    train_prop: float,
+    seed: int,
+    batch_size: int, 
+    num_workers: int, 
+    pin_memory: False
+) -> None:
         super().__init__()
-        self.hparams = hparams
+        self.save_hyperparameters(logger=False)
+        self.train_path = data_dir / "train"
+        self.test_path = data_dir / "test"
+        self.seed = seed
         self.data_train: Optional[LFW] = None
         self.data_val: Optional[LFW] = None
         self.data_test: Optional[LFW] = None
@@ -19,8 +30,8 @@ class FaceTripletsDataModule(pl.LightningDataModule):
         
     def setup(self) -> None:
         if not self.data_train and not self.data_val and not self.data_test:
-            trainset = LFW(self.hparams.train_path, transform=None)
-            testset = LFW(self.hparams.test_path, transform=None)
+            trainset = LFW(self.train_path, transform=None)
+            testset = LFW(self.test_path, transform=None)
             lengths = [int(len(trainset)*self.hparams.train_prop)+1, int(len(trainset)*(1-self.hparams.train_prop))]
             self.data_test = testset
             self.data_train, self.data_val = random_split(
