5a7b9356a8c0d1dbe98b87645c4611c34f658615
victoria-brami <victoria.brami88@gmail.com>
2023-01-19 18:47:07 +0100

    restored valid commit

--- facenet/datasets/lfw.py
+++ facenet/datasets/lfw.py
@@ -60,8 +60,8 @@ class LFW(Dataset):
         anchor_image = self.transform(load_image(anchor))
         positive_image = self.transform(load_image(positive))
         negative_image = self.transform(load_image(negative))
-        dist_pos = torch.linalg.norm(self.facenet(anchor_image) - self.facenet(positive_image))
-        dist_neg = torch.linalg.norm(self.facenet(anchor_image) - self.facenet(negative_image))
+        dist_pos = torch.linalg.norm(self.facenet(anchor_image[None, :]) - self.facenet(positive_image[None, :]))
+        dist_neg = torch.linalg.norm(self.facenet(anchor_image[None, :]) - self.facenet(negative_image[None, :]))
         
         iteration = 0
         while ( dist_pos**2 > dist_neg**2) and iteration < 10:
@@ -71,7 +71,7 @@ class LFW(Dataset):
             neg_id = np.random.choice(nb_neg_images, 1, replace=False)[0]
             negative =  self._person_paths[neg_name][neg_id]
             negative_image = self.transform(load_image(negative))
-            dist_neg = torch.linalg.norm(self.facenet(anchor_image) - self.facenet(negative_image))
+            dist_neg = torch.linalg.norm(self.facenet(anchor_image[None, :]) - self.facenet(negative_image[None, :]))
             #iteration += 1
 
         return anchor, positive, negative
