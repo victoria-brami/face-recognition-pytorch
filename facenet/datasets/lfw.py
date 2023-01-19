52e14cf5da0cdb90806fbee0616caae352f5d788
victoria-brami <victoria.brami88@gmail.com>
2023-01-19 15:38:29 +0100

    hard triplet negative implementation

--- facenet/datasets/lfw.py
+++ facenet/datasets/lfw.py
@@ -59,12 +59,15 @@ class LFW(Dataset):
         
         dist_pos = torch.linalg.norm(self.facenet(anchor) - self.facenet(positive))
         dist_neg = torch.linalg.norm(self.facenet(anchor) - self.facenet(negative))
-        while ( dist_pos**2 > dist_neg**2):
+        
+        iteration = 0
+        while ( dist_pos**2 > dist_neg**2) and iteration < 10:
             neg_name = np.random.choice(neg_names, 1)[0]
             nb_neg_images = len(self._person_paths[neg_name])
             neg_id = np.random.choice(nb_neg_images, 1, replace=False)[0]
             negative =  self._person_paths[neg_name][neg_id]
             dist_neg = torch.linalg.norm(self.facenet(anchor) - self.facenet(negative))
+            iteration += 1
 
         return anchor, positive, negative
 
